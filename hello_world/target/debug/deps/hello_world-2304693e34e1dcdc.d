8f18e8f0b2e0bef60c7a77e10706b82ac2ef0ff1
kshashi716 <kshashi716@gmail.com>
2024-08-04 21:01:58 +0530

    first commit

---
+++ hello_world/target/debug/deps/hello_world-2304693e34e1dcdc.d
@@ -0,0 +1,5 @@
+/home/shashi/rust/projects/rustprojects/hello_world/target/debug/deps/hello_world-2304693e34e1dcdc: src/main.rs
+
+/home/shashi/rust/projects/rustprojects/hello_world/target/debug/deps/hello_world-2304693e34e1dcdc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/shashi/rust/projects/rustprojects/hello_world/target/debug/hello_world: /home/shashi/rust/projects/rustprojects/hello_world/src/main.rs
